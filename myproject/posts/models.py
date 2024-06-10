46a6fc25dead6f3a25fb9cbcb9296e3aafc77f3e
Calvin Yiu <chunnyyiu@hotmail.com>
2024-06-10 23:47:26 +0100

    ORM experimenting

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -5,5 +5,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
-    slug = models.SlugField(max_length=75, unique=True)
+    slug = models.SlugField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
